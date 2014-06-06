a41074b42c54a0fb5a9898dcc9c45282f30d6764
Mauricio Beltran <maobel@hotmail.com>
2014-06-05 23:25:24 -0500

    Update

---
+++ app/code/community/Clarion/Customerattribute/Block/Adminhtml/Customerattribute/Edit/Tab/Options.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Product attribute add/edit form options tab
+ * 
+ * @category    Clarion
+ * @package     Clarion_Customerattribute
+ * @author      Clarion Magento Team
+ */
+class Clarion_Customerattribute_Block_Adminhtml_Customerattribute_Edit_Tab_Options extends Clarion_Customerattribute_Block_Adminhtml_Customerattribute_Edit_Options_Abstract
+{
+}
